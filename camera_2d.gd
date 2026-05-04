9e55ef0ff6a4150ce1a77e74635fa98265927406
Eric Inui <einui@showcasepro.com.br>
2026-05-04 17:34:04 -0300

    feature: add a small slopped platform

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,9 +1,9 @@
 extends Camera2D
 
-# Limit camera so it does not show anything below Y = 500
+# Limit camera so it does not show anything below Y = 1000
 func _limit_camera_y() -> void:
-	if global_position.y > 500:
-		global_position.y = 500
+	if global_position.y > 1000:
+		global_position.y = 1000
 
 func _ready() -> void:
 	_limit_camera_y()
